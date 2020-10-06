7982b1c204d6f878d645ac0343959e21d5b82c02
François Paupier <francois.paupier@gmail.com>
2020-10-06 14:18:07 +0200

    add: sample credentials file

---
+++ credentials-template.go
@@ -0,0 +1,16 @@
+package main
+
+const (
+	//  Email
+	apiPublicKeyEmail  = "Your Api public Key"
+	apiPrivateKeyEmail = "Your Api private Key"
+	//  Kafka
+	ConfluentServer = "Your Confluent Server"
+	ConfluentApiKey = "Your Confluent API Key"
+	ConfluentSecret = "Your Confluent secret"
+	// PostgresSQL GCP
+	InstanceConnectionName = "Your Cloud SQL instance name"
+	DatabaseName           = "your db name"
+	DatabaseUser           = "your db user"
+	Password               = "your db user's password"
+)
