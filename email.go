aa80fd0b7e772439e158652f21028bb4f948d394
François Paupier <francois.paupier@gmail.com>
2020-10-06 15:27:41 +0200

    add: document email

--- email.go
+++ email.go
@@ -6,19 +6,25 @@ import (
 	"log"
 )
 
-func sendEmail(to, ts string, id int) {
+const (
+	senderEmail = "l6yaldbk6@relay.firefox.com" // the sender's email address can be loaded from a config file or fetched from db
+	senderName  = "Francis"                     //  name can also be loaded from config or fetched from a DB
+)
+
+// sendEmail formats an email sent to recipientEmail to inform them that an alert is raised at time ts, indicating the id of the alert.
+func sendEmail(recipientEmail, ts string, id int) {
 	mailjetClient := mailjet.NewMailjetClient(apiPublicKeyEmail, apiPrivateKeyEmail)
 	body := fmt.Sprintf("<h3>Alert</h3><br />Someone has been seen not wearing their mask at %s. Check alert ID#%d for more information.", ts, id)
 	messagesInfo := []mailjet.InfoMessagesV31{
 		{
 			From: &mailjet.RecipientV31{
-				Email: "l6yaldbk6@relay.firefox.com",
-				Name:  "Francis",
+				Email: senderEmail,
+				Name:  senderName,
 			},
 			To: &mailjet.RecipientsV31{
 				mailjet.RecipientV31{
-					Email: to,
-					Name:  "Francis",
+					Email: recipientEmail,
+					Name:  senderName,
 				},
 			},
 			Subject:  "Mask Alert",
